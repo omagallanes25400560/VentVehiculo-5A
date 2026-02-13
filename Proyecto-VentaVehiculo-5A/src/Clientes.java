635811f957855713dfd56c68bc7d719ac69dec15
omagallanes25400560 <oljamagallanesto@ittepic.edu.mx>
2026-02-13 11:22:06 -0700

    Clases terminadas con set y get

--- Proyecto-VentaVehiculo-5A/src/Clientes.java
+++ Proyecto-VentaVehiculo-5A/src/Clientes.java
@@ -1,18 +1,43 @@
 
 public class Clientes {
+
     private String rs;
     private String rfc;
     private String email;
-    
-    public Clientes(String rs,String rfc,String email){
-    this.rs=rs;
-    this.rfc=rfc;
-    this.email=email;
+
+    public Clientes(String rs, String rfc, String email) {
+        this.rs = rs;
+        this.rfc = rfc;
+        this.email = email;
     }
-    @Override 
-    public String toString(){
-    return "Rs: "+this.rs+"\n"+
-            "Rfc: "+this.rfc+"\n"+
-            "email: "+this.email+"\n";
+
+    //Rs
+    public void setrs(String rs) {
+        this.rs = rs;
+    }
+
+    public String getrs() {
+        return this.rs;
+    }
+
+    //RFC
+    public void setrfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getrfc() {
+        return this.rfc;
+    }
+
+    //email
+    public void setemail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Rs: " + this.rs + "\n"
+                + "Rfc: " + this.rfc + "\n"
+                + "email: " + this.email + "\n";
     }
 }
--- Proyecto-VentaVehiculo-5A/src/Vehiculo.java
+++ Proyecto-VentaVehiculo-5A/src/Vehiculo.java
@@ -1,23 +1,47 @@
+
 public class Vehiculo {
-    
+
     private String marca;
     private double precio;
     private String motor;
-    
-    public Vehiculo(String marca,double precio,String motor){
-    this.marca=marca;
-    this.precio=precio;
-    this.motor=motor;
+
+    public Vehiculo(String marca, double precio, String motor) {
+        this.marca = marca;
+        this.precio = precio;
+        this.motor = motor;
     }
-    public void setmarca(String marca){
-    this.marca=marca;
+
+    //marca
+    public void setmarca(String marca) {
+        this.marca = marca;
     }
-    
-    
+
+    public String getmarca() {
+        return this.marca;
+    }
+
+    //Precio
+    public void setprecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getprecio() {
+        return this.precio;
+    }
+
+    //Motor
+    public void setmotor(String motor) {
+        this.motor = motor;
+    }
+
+    public String getmotor() {
+        return this.motor;
+    }
+
     @Override
-    public String toString(){
-    return "Marca: "+this.marca+"\n"+
-            "Precio: "+this.precio+"\n"+
-            "Motor: "+this.motor+"\n";
+    public String toString() {
+        return "Marca: " + this.marca + "\n"
+                + "Precio: " + this.precio + "\n"
+                + "Motor: " + this.motor + "\n";
     }
 }
